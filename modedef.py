6e25bdccd0f7f1bd8b7b8b40e8f0cb330e1e2688
fg8oj <41483500+fg8oj@users.noreply.github.com>
2019-04-12 21:55:00 -0400

    Add files via upload

---
+++ modedef.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Script FG8OJ pour Spotnik
+# Relance après inactivité radio vers un groupe par défault
+# Cron :
+# * * * * * python /etc/spotnik/modedef.py > /dev/null 2>&1
+# Configuration :
+default="971" # appel du fichier restart /etc/spotnik/restart.XXX
+timeout=60 # durée en minute avant le rebasculement sur le salon par défault
+#
+#
+import datetime
+import os
+log = open("/etc/spotnik/network","rt")
+reseau=log.readline().strip()
+if (reseau == default):
+ exit()
+log2 = open("/tmp/svxlink.log", "rt")
+delay=9999999
+for x in log2:
+ if ( (x.find("The squelch is OPEN")>-1)or(x.find("Activating link")>-1) ):
+  print(x)
+  dti=x.find(": ")
+  dt=x[:dti]
+  dt= datetime.datetime.strptime(dt,"%a %b %d %H:%M:%S %Y")
+  dt=datetime.datetime.now() - dt
+  if (delay>dt.total_seconds()):
+   delay=int(dt.total_seconds())
+if (delay==9999999):
+ print("exit 99999")
+ exit()
+print(delay)
+if (delay>(timeout*60)):
+ print("restart "+ str(delay))
+ os.system("/etc/spotnik/restart." + str(default)) 
+log.close()
+ 
