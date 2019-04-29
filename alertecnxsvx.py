384c5c8bd3e4c4d9a507521617be24e584746e83
fg8oj <41483500+fg8oj@users.noreply.github.com>
2019-04-29 11:55:54 -0400

    Update alertecnxsvx.py

--- alertecnxsvx.py
+++ alertecnxsvx.py
@@ -1,3 +1,14 @@
+#!interpreter [optional-arg]
+# coding: utf-8
+
+
+########################################################
+####    Flux connexion/deconnexion svxlinkreflector ####
+########################################################
+# FG8OJ sous licence MIT
+# A faire tourner dans un cron => * * * * * python /root/alertecnxsvx.py > /dev/null 2>&1
+# 
+
 import os
 import datetime
 import time
@@ -7,9 +18,7 @@ import re
 for i in range(0,6) :
  dtlog= datetime.datetime.now()-datetime.timedelta(seconds=10)
  dtlog= dtlog.strftime("%b %d %H:%M:%S")[:-1]
-# dtlog= dtlog.strftime("%b ")[:-1]
  cat='cat /var/log/syslog|grep "'+dtlog+ '"|grep "svxreflector svxreflector"|grep -E  "Login OK|Connection closed|Heartbeat timeout"'
- print(cat)
  ex=os.popen(cat).read().split("\n")
  for li in ex:
   if (len(li)>0):
@@ -24,8 +33,6 @@ for i in range(0,6) :
    li2=li2[0].split(")")
    if (len(li2)>0):
     li2=li2[1].strip().replace(" ","-")
-#    li2=li2.split("-")
-#    li2=li2[0].strip()
     r=li2.strip()
    ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', li )
    if (len(ip)>0):
